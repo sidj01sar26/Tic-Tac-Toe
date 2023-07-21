7a940238906aedb2018b32203dd26d42ceca46b7
Siddharth Joisar <sidjoisar0807@gmail.com>
2023-07-21 17:35:17 +0530

    Update

--- index.js
+++ index.js
@@ -51,44 +51,44 @@ function checkGameOver() {
     let answer = "";
 
     winningPositions.forEach((position) => {
-        //all 3 boxes should be non-empty and exactly same in value
+        // All 3 boxes should be non-empty and exactly same in value
         if ((gameGrid[position[0]] !== "" || gameGrid[position[1]] !== "" || gameGrid[position[2]] !== "")
             && (gameGrid[position[0]] === gameGrid[position[1]]) && (gameGrid[position[1]] === gameGrid[position[2]])) {
 
-            //check if winner is X
+            // Check if X won
             if (gameGrid[position[0]] === "X")
                 answer = "X";
             else {
                 answer = "O";
             }
 
-            //disable pointer events
+            // Disable pointer events
             boxes.forEach((box) => {
                 box.style.pointerEvents = "none";
             })
 
-            //now we know X/O is a winner
+            // Now we know that X/O is a winner
             boxes[position[0]].classList.add("win");
             boxes[position[1]].classList.add("win");
             boxes[position[2]].classList.add("win");
         }
     });
 
-    //it means we have a winner
+    // Tt means we have a winner
     if (answer !== "") {
         gameInfo.innerText = `Winner Player - ${answer}`;
         newGameBtn.classList.add("active");
         return;
     }
 
-    //We know, NO Winner Found, let's check whether there is tie
+    // We know, NO Winner Found, let's check whether there is tie
     let fillCount = 0;
     gameGrid.forEach((box) => {
         if (box !== "")
             fillCount++;
     });
 
-    //board is full, game is TIE
+    // Board is full, game is TIE
     if (fillCount === 9) {
         gameInfo.innerText = "Game Tied !";
         newGameBtn.classList.add("active");
@@ -101,9 +101,9 @@ function handleClick(index) {
         boxes[index].innerText = currentPlayer;
         gameGrid[index] = currentPlayer;
         boxes[index].style.pointerEvents = "none";
-        //swap karo turn ko
+        // Swap & Turn
         swapTurn();
-        //check koi jeet toh nahi gya
+        // Check did someone win
         checkGameOver();
     }
 }
